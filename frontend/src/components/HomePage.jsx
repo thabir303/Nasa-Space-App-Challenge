3e8ef733fd2bd769397b101a9dac44c415df5c85
th_abir303 <bsse1321@iit.du.ac.bd>
2024-10-04 23:44:35 +0600

    change some names

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white text-center">
-      <h1 className="text-4xl font-bold mb-6">Welcome to My Educational Website</h1>
+      <h1 className="text-4xl font-bold mb-6">Welcome to PACExplorer</h1>
       <p className="text-lg text-gray-700 mb-8">
         Learn about various global phenomena and data visualization techniques. Click the buttons below to explore different topics.
       </p>
